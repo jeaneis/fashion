599bcdcb9ed183f2fed376af6425d7bd682e2881
Sharon <shirokiryu@gmail.com>
2013-09-29 05:20:35 -0700

    Modifying files, replacing jars

--- src/digicloset/colors/FlickrImages.java
+++ src/digicloset/colors/FlickrImages.java
@@ -1,12 +1,16 @@
 package digicloset.colors;
-import com.flickr4java.flickr.*;
+import com.googlecode.flickrjandroid.*;
+import com.googlecode.flickrjandroid.favorites.*;
+import com.googlecode.flickrjandroid.photos.*;
+/*import com.flickr4java.flickr.*;
 import com.flickr4java.flickr.auth.*;
 import com.flickr4java.flickr.favorites.FavoritesInterface;
-import com.flickr4java.flickr.photos.*;
+import com.flickr4java.flickr.photos.*; */
 import java.awt.image.*;
 import java.io.File;
 import javax.imageio.*;
-import com.flickr4java.flickr.util.IOUtilities;
+import com.googlecode.flickrjandroid.photos.Photo;
+//import com.flickr4java.flickr.util.IOUtilities;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -41,7 +45,7 @@ public class FlickrImages {
                 properties = new Properties();
                 properties.load(in);
             } finally {
-                IOUtilities.close(in);
+                in.close();
             }
 
 
@@ -51,22 +55,17 @@ public class FlickrImages {
 
             Flickr flickr = new Flickr(apiKey, secret, new REST());
             requestContext = RequestContext.getRequestContext();
-            Auth auth = new Auth();
-            auth.setPermission(Permission.READ);
-            auth.setToken("token");
-            requestContext.setAuth(auth);
-            Flickr.debugRequest = false;
-            Flickr.debugStream = false;
+
 
             favorites = flickr.getFavoritesInterface();
             photoInterface = flickr.getPhotosInterface();
 
-            PhotoList<Photo> photos = favorites.getPublicList(user, 10,1, null);
+
+            PhotoList photos = favorites.getPublicList(user, null, null, 10,1, null);
             for (int i=0; i<photos.size(); i++)
             {
-                BufferedImage image = photoInterface.getImage(photos.get(i), 240);
+                BufferedImage image = ImageIO.read(photoInterface.getImageAsStream(photos.get(i), 250));
                 ImageIO.write(image, "png", new File("flickr"+i+".png"));
-
             }
         }
         catch (Exception e)
