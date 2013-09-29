a1501c88da765607215eaeb3a4158f0d01fd4d43
Sharon <shirokiryu@gmail.com>
2013-09-29 06:50:25 -0700

    Flickr image scoring!

--- src/digicloset/colors/ColorUtils.java
+++ src/digicloset/colors/ColorUtils.java
@@ -19,6 +19,39 @@ import java.io.*;
  * To change this template use File | Settings | File Templates.
  */
 public class ColorUtils {
+
+    public static double PaletteDist(LAB[] a, LAB[] b)
+    {
+        //create the matrix
+        assert(a.length == b.length);
+        int n = a.length;
+        float[][] matrix = new float[n][n];
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<n; j++)
+            {
+                matrix[i][j] = (float)SqDist(a[i], b[j]);
+            }
+        }
+        HungarianAlgorithm ha = new HungarianAlgorithm();
+
+        double error = 0;
+        int[][] assignments = ha.computeAssignments(matrix);
+
+        for (int i=0; i<n; i++)
+            error += SqDist(a[assignments[i][0]], b[assignments[i][1]]);
+        error /= (n*10000);
+        return error;
+    }
+
+    public static LAB[] RGBsToLAB(Color[] rgbs)
+    {
+       LAB[] labs = new LAB[rgbs.length];
+       for (int i=0; i<rgbs.length; i++)
+           labs[i] = new LAB(rgbs[i]);
+       return labs;
+    }
+
     public static double SqDist(Color a, Color b)
     {
         return Math.pow(a.getRed()-b.getRed(),2) + Math.pow(a.getGreen()-b.getGreen(),2) + Math.pow(a.getBlue()-b.getBlue(),2);
--- src/digicloset/colors/FlickrImages.java
+++ src/digicloset/colors/FlickrImages.java
@@ -2,15 +2,12 @@ package digicloset.colors;
 import com.googlecode.flickrjandroid.*;
 import com.googlecode.flickrjandroid.favorites.*;
 import com.googlecode.flickrjandroid.photos.*;
-/*import com.flickr4java.flickr.*;
-import com.flickr4java.flickr.auth.*;
-import com.flickr4java.flickr.favorites.FavoritesInterface;
-import com.flickr4java.flickr.photos.*; */
+
+import java.awt.*;
 import java.awt.image.*;
 import java.io.File;
 import javax.imageio.*;
-import com.googlecode.flickrjandroid.photos.Photo;
-//import com.flickr4java.flickr.util.IOUtilities;
+import com.googlecode.flickrjandroid.people.*;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -29,14 +26,17 @@ public class FlickrImages {
     private String apiKey;
     private String secret;
     private String token;
-    RequestContext requestContext;
-    FavoritesInterface favorites;
-    PhotosInterface photoInterface;
-    Properties properties;
+    private RequestContext requestContext;
+    private FavoritesInterface favorites;
+    private PhotosInterface photoInterface;
+    private Properties properties;
+    private PeopleInterface users;
+    private ArrayList<BufferedImage> images;
+    private ArrayList<LAB[]> palettes;
 
-
-    public FlickrImages(String user)
+    public FlickrImages(String userName, int numImages)
     {
+        System.out.println("Loading Flickr favorites from " + userName);
         try
         {
             InputStream in = null;
@@ -53,32 +53,72 @@ public class FlickrImages {
             secret = properties.getProperty("secret");
             token = properties.getProperty("token");
 
-            Flickr flickr = new Flickr(apiKey, secret, new REST());
-            requestContext = RequestContext.getRequestContext();
+            images = new ArrayList<BufferedImage>();
+            palettes = new ArrayList<LAB[]>();
 
+            Flickr flickr = new Flickr(apiKey, secret);
+            requestContext = RequestContext.getRequestContext();
 
             favorites = flickr.getFavoritesInterface();
             photoInterface = flickr.getPhotosInterface();
+            users = flickr.getPeopleInterface();
 
+            User user = users.findByUsername(userName);
+            //"58435973@N07"
+            PhotoList photos = favorites.getPublicList(user.getId(), null, null, numImages,1, null);
+            //PhotoList photos = favorites.getList(user, null, null, 0,0, null);
 
-            PhotoList photos = favorites.getPublicList(user, null, null, 10,1, null);
             for (int i=0; i<photos.size(); i++)
             {
-                BufferedImage image = ImageIO.read(photoInterface.getImageAsStream(photos.get(i), 250));
-                ImageIO.write(image, "png", new File("flickr"+i+".png"));
+                BufferedImage image = ImageIO.read(photoInterface.getImageAsStream(photos.get(i), Size.THUMB));
+                images.add(image);
             }
+            ComputePalettes();
         }
         catch (Exception e)
         {
             System.out.println("Can't download images");
             e.printStackTrace();
+            ComputePalettes();
         }
+    }
 
+    private void ComputePalettes()
+    {
+        for (int i=0; i<images.size(); i++)
+        {
+            Color[] palette = ColorClustering.KMeans(images.get(i),5,5);
+            LAB[] labs = new LAB[palette.length];
+            for (int c=0; c<palette.length; c++)
+            {
+                labs[c] = new LAB(palette[c]);
+            }
+            palettes.add(labs);
+        }
+        System.out.println("Computed Palettes");
+    }
+
+    public double Score(LAB[] query)
+    {
+        double score = 0;
+        for (int i=0; i<palettes.size(); i++)
+        {
+            score += ColorUtils.PaletteDist(palettes.get(i), query);
+        }
+        score /= palettes.size();
+
+        return Math.exp(-score);
     }
 
     public static void main(String[] args)
     {
-        FlickrImages images = new FlickrImages("craig.denford");
+
+        FlickrImages inventory = new FlickrImages("Vic Powles", 5);
+        Color[] palette = {new Color(255,0,0), new Color(0,0,0), new Color(0,10,40), new Color(200,120,120), new Color(100, 200, 240)};
+        LAB[] labs = ColorUtils.RGBsToLAB(palette);
+        System.out.println("Score " + inventory.Score(labs));
+
+
     }
 
 
